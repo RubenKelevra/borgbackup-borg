fdfb3ffe7753d17b1dbd891b17296bac13d1725c
@RubenKelevra <cyrond@gmail.com>
2023-01-29 19:53:12 +0100

    fix typos

--- src/borg/archiver/check_cmd.py
+++ src/borg/archiver/check_cmd.py
@@ -13,13 +13,13 @@ logger = create_logger()
 class CheckMixIn:
     @with_repository(exclusive=True, manifest=False)
     def do_check(self, args, repository):
-        """Check repository consistency"""
+        """Check repository's consistency"""
         if args.repair:
             msg = (
                 "This is a potentially dangerous function.\n"
                 "check --repair might lead to data loss (for kinds of corruption it is not\n"
                 "capable of dealing with). BE VERY CAREFUL!\n"
-                "Can be permanently confirmed by enviroment variable BORG_CHECK_I_KNOW_WHAT_I_AM_DOING.\n"
+                "Can be permanently confirmed by environment variable BORG_CHECK_I_KNOW_WHAT_I_AM_DOING.\n"
                 "\n"
                 "Type 'YES' if you understand this and want to continue: "
             )
@@ -34,17 +34,17 @@ class CheckMixIn:
                 return EXIT_ERROR
         if args.repo_only and any((args.verify_data, args.first, args.last, args.match_archives)):
             self.print_error(
-                "--repository-only contradicts --first, --last, -a / --match-archives and --verify-data arguments."
+                "--repository-only contradicts --first, --last, -a/--match-archives and --verify-data arguments."
             )
             return EXIT_ERROR
         if args.repair and args.max_duration:
             self.print_error("--repair does not allow --max-duration argument.")
             return EXIT_ERROR
         if args.max_duration and not args.repo_only:
-            # when doing a partial repo check, we can only check crc32 checksums in segment files,
+            # When doing a partial repo check, we can only check crc32 checksums in segment files,
             # we can't build a fresh repo index in memory to verify the on-disk index against it.
-            # thus, we should not do an archives check based on a unknown-quality on-disk repo index.
-            # also, there is no max_duration support in the archives check code anyway.
+            # Thus, we should not do archive checks based on a unknown-quality on-disk repo index.
+            # Also, there is no max_duration support in the archives check code anyway.
             self.print_error("--repository-only is required for --max-duration support.")
             return EXIT_ERROR
         if not args.archives_only:
@@ -78,7 +78,7 @@ class CheckMixIn:
         check --repair is a potentially dangerous function and might lead to data loss
         (for kinds of corruption it is not capable of dealing with). BE VERY CAREFUL!
 
-        Pursuant to the previous warning it is also highly recommended to test the
+        Pursuant to the previous warning, it is also highly recommended to test the
         reliability of the hardware running this software with stress testing software
         such as memory testers. Unreliable hardware can also lead to data loss especially
         when this command is run in repair mode.
@@ -96,7 +96,7 @@ class CheckMixIn:
         - If checking a remote repo via ``ssh:``, the repo check is executed on the server
           without causing significant network traffic.
         - The repository check can be skipped using the ``--archives-only`` option.
-        - A repository check can be time consuming. Partial checks are possible with the
+        - A repository check can be time-consuming. Partial checks are possible with the
           ``--max-duration`` option.
 
         Second, the consistency and correctness of the archive metadata is verified:
@@ -120,11 +120,11 @@ class CheckMixIn:
         - If checking a remote repo via ``ssh:``, the archive check is executed on the
           client machine because it requires decryption, and this is always done client-side
           as key access is needed.
-        - The archive checks can be time consuming; they can be skipped using the
+        - The archive checks can be time-consuming; they can be skipped using the
           ``--repository-only`` option.
 
         The ``--max-duration`` option can be used to split a long-running repository check
-        into multiple partial checks. After the given number of seconds the check is
+        into multiple partial checks. After the given number of seconds, the check is
         interrupted. The next partial check will continue where the previous one stopped,
         until the complete repository has been checked. Example: Assuming a complete check took 7
         hours, then running a daily check with --max-duration=3600 (1 hour) resulted in one
@@ -134,7 +134,7 @@ class CheckMixIn:
         --repository-only check: only the non-cryptographic checksum checks on segment file
         entries are done, while a full --repository-only check would also do a repo index check.
         A partial check cannot be combined with the ``--repair`` option. Partial checks
-        may therefore be useful only with very large repositories where a full check would take
+        may therefore be useful only with very large repositories, where a full check would take
         too long.
         Doing a full repository check aborts a partial check; the next partial check will restart
         from the beginning.
@@ -142,7 +142,7 @@ class CheckMixIn:
         The ``--verify-data`` option will perform a full integrity verification (as opposed to
         checking the CRC32 of the segment) of data, which means reading the data from the
         repository, decrypting and decompressing it. This is a cryptographic verification,
-        which will detect (accidental) corruption. For encrypted repositories it is
+        which will detect (accidental) corruption. For encrypted repositories, it is
         tamper-resistant as well, unless the attacker has access to the keys. It is also very
         slow.
         """
